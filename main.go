9eb672f375d00a7d70a6b2f1b51eaa23f8cd763c
Spencer Peterson <spencerjpeterson@gmail.com>
2024-03-22 20:52:06 -0700

    Remove debug password gen from main

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -15,10 +12,6 @@ import (
 var db DB
 
 func main() {
-	if len(os.Args) > 1 {
-		fmt.Printf("MY_PASSWORD=%s\n", b64(auth.SaltAndHash(os.Args[1])))
-	}
-
 	var err error
 	db, err = NewDB(os.Getenv("DB_FILE"))
 	if err != nil {
@@ -34,27 +27,17 @@ func main() {
 	http.Handle("POST /login", auth.PostLogin(usersDB))
 
 	http.Handle("/{$}", http.RedirectHandler(filepath.Join(os.Getenv("PATH_PREFIX"), "/search"), http.StatusFound))
-	authhandle("/search", search)
+	authhandle("/search", makeSearch())
 
 	authhandle("POST /pages", scrapePage)
 	authhandle("GET /pages/{id}", notImpl)
 	authhandle("DELETE /pages/{id}", notImpl)
+
+	http.Handle("GET /static/", http.FileServer(http.FS(staticContent)))
 
 	log.Errorf("listen and serve: %v", http.ListenAndServe(":6844", nil))
 }
 
 func notImpl(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Not Implemented", http.StatusNotImplemented)
-}
-
-func b64(in []byte) []byte {
-	var b bytes.Buffer
-	enc := base64.NewEncoder(base64.URLEncoding, &b)
-	if _, err := enc.Write(in); err != nil {
-		panic(err)
-	}
-	if err := enc.Close(); err != nil {
-		panic(err)
-	}
-	return b.Bytes()
 }
