63fa03a432aebd87bf0c35ac5e1bae7d64fd5dd8
Spencer Peterson <spencerjpeterson@gmail.com>
2024-03-22 19:38:30 -0700

    log.Printf -> log.Infof

--- handlers.go
+++ handlers.go
@@ -22,12 +22,12 @@ func scrapePage(w http.ResponseWriter, r *http.Request) {
 	var content PostPageRequest
 	if err := json.NewDecoder(r.Body).Decode(&content); err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		log.Printf("POST /pages: Failed to decode JSON: %v", err)
+		log.Infof("POST /pages: Failed to decode JSON: %v", err)
 		return
 	}
 	if content.URL == "" || content.Title == "" || content.TextContent == "" {
 		http.Error(w, "Incomplete request", http.StatusBadRequest)
-		log.Printf("POST /pages: Incomplete request. URL=%t, title=%t, content=%t",
+		log.Infof("POST /pages: Incomplete request. URL=%t, title=%t, content=%t",
 			content.URL == "", content.Title == "", content.TextContent == "")
 		return
 	}
@@ -35,7 +35,7 @@ func scrapePage(w http.ResponseWriter, r *http.Request) {
 	location, err := url.Parse(content.URL)
 	if err != nil {
 		http.Error(w, "Bad URL", http.StatusBadRequest)
-		log.Printf("POST /pages: Bad URL %q", content.URL)
+		log.Infof("POST /pages: Bad URL %q", content.URL)
 		return
 	}
 	minLocation := url.URL{
@@ -54,7 +54,7 @@ func scrapePage(w http.ResponseWriter, r *http.Request) {
 	id, err := db.Save(col)
 	if err != nil {
 		http.Error(w, "Failed to commit data", http.StatusInternalServerError)
-		log.Printf("POST /pages: Failed to save in DB: %v", err)
+		log.Infof("POST /pages: Failed to save in DB: %v", err)
 		return
 	}
 
@@ -76,7 +76,7 @@ func search(w http.ResponseWriter, r *http.Request) {
 	results, err := db.Search(query)
 	if err != nil {
 		http.Error(w, "Failed to query database", http.StatusInternalServerError)
-		log.Printf("search: failed to query for %q: %v", query, err)
+		log.Infof("search: failed to query for %q: %v", query, err)
 		return
 	}
 
