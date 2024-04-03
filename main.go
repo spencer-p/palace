d4c7b54ceb09c6d860b2bbaa6a09aaa8a2f7c34a
Spencer Peterson <spencerjpeterson@gmail.com>
2024-04-03 11:02:07 -0700

    Log HTTP request info

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/spencer-p/palace/pkg/auth"
@@ -18,18 +19,19 @@ func main() {
 		log.Fatalf("Prepare database: %v", err)
 	}
 
+	mux := http.NewServeMux()
 	usersDB := fakeUsersDB{}
 	authhandle := func(path string, f func(w http.ResponseWriter, r *http.Request)) {
-		http.Handle(path, auth.OnlyAuthenticated(usersDB, http.HandlerFunc(f)))
+		mux.Handle(path, auth.OnlyAuthenticated(usersDB, http.HandlerFunc(f)))
 	}
 
-	http.HandleFunc("GET /login", auth.GetLogin)
-	http.Handle("POST /login", auth.PostLogin(usersDB))
+	mux.HandleFunc("GET /login", auth.GetLogin)
+	mux.Handle("POST /login", auth.PostLogin(usersDB))
 
-	http.Handle("/{$}", http.RedirectHandler(filepath.Join(os.Getenv("PATH_PREFIX"), "/search"), http.StatusFound))
+	mux.Handle("/{$}", http.RedirectHandler(filepath.Join(os.Getenv("PATH_PREFIX"), "/search"), http.StatusFound))
 	authhandle("/search", makeSearch())
 
-	http.HandleFunc("OPTIONS /pages", scrapePageOptions)
+	mux.HandleFunc("OPTIONS /pages", scrapePageOptions)
 	authhandle("POST /pages", scrapePage)
 	authhandle("GET /pages/{id}", notImpl)
 	authhandle("DELETE /pages/{id}", notImpl)
@@ -37,9 +39,18 @@ func main() {
 	http.Handle("GET /static/", http.FileServer(http.FS(staticContent)))
 
 	log.Infof("Starting server")
-	log.Errorf("listen and serve: %v", http.ListenAndServe(":6844", nil))
+	log.Errorf("listen and serve: %v", http.ListenAndServe(":6844", logWrap(mux)))
 }
 
 func notImpl(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+}
+
+func logWrap(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tstart := time.Now()
+		method, path := r.Method, r.URL
+		next.ServeHTTP(w, r)
+		log.Info("Served HTTP", "method", method, "path", path, "useragent", r.Header.Get("User-Agent"), "latency", time.Now().Sub(tstart))
+	})
 }
